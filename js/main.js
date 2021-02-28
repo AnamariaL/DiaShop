5fde3674cd2fe9d15852cbd28f6734af601d7c58
AnamariaL <anamarialilea7@gmail.com>
2021-02-28 22:23:53 +0200

    Ana

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+const responsive = {
+    0:{
+        items:1
+    },
+    320:{
+        items:1
+    },
+    560:{
+        items:2
+    },
+    960:{
+        items:3
+    }
+
+}
+
 $(document).ready(function(){
     $nav = $('.nav');
     $toggleCollapse = $('.toggle-Collapse');
@@ -5,5 +21,26 @@ $(document).ready(function(){
     /**click event on toggle menu*/
     $toggleCollapse.click(function(){
         $nav.$toggleCollapse('collapse');
+    })
+    
+    // owl-carousel for adv
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        dots:false,
+        nav:true,
+        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+        responsive: responsive
     });
+
+    //click to scroll up
+    $('.move-up span').click(function(){
+        $('html,body').animate({
+            scrollTop:0
+        },1000);
+    })
+
 });
+
